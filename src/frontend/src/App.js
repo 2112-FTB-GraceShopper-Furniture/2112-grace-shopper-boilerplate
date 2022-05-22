b41dbab92b0ccfb6ab159df8b40dea0eb88b2630
SravanthiN4 <snedunoori44@gmail.com>
2022-05-22 01:01:12 -0500

    frontend issue

---
+++ src/frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header'
+
+
+const App = () => {
+    
+    return (
+        <>
+            <h1>
+                Hello World!
+            </h1>
+            <Header/>
+        
+        </>
+    );
+};
+
+export default App;
