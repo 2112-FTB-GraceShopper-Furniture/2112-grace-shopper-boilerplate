f33a22523177ac9628e7d166c7efe1b58fb0d355
Ramses Angles <ramsescangles@gmail.com>
2022-06-04 11:10:43 -0500

    working on adding products to cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/Product.css";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+
+const isUserLoggedIn = true; // change this later
 
 function Product({ id, title, image, price, rating, alt }) {
+  const [activeCart, setActiveCart] = useState([]);
+
+  const handleAddProduct = (product) => {
+    //check if product is already in cart and update quantity
+    const ProductExist = activeCart.find((item) => item.id === product.id);
+    //if product is in cart, update quantity  else add product to cart
+    if (ProductExist) {
+      if (isUserLoggedIn) {
+        //increase quantity in database
+      } else {
+        // increase quantity in localstorage
+      }
+    } else {
+      if (isUserLoggedIn) {
+        // add to cart in database
+      } else {
+        // add to cart in localstorage
+      }
+    }
+
+    //   setActiveCart(
+    //     activeCart.map((item) =>
+    //       item.id === product.id
+    //         ? { ...item, quantity: item.quantity + 1 }
+    //         : item
+    //     )
+    //   );
+    // } else {
+    //   setActiveCart([...activeCart, { ...product, quantity: 1 }]);
+    // }
+  };
+
   return (
     <div className="product" key={id}>
       <div className="product__info">
@@ -20,10 +54,11 @@ function Product({ id, title, image, price, rating, alt }) {
         </div>
       </div>
 
-      <Link to = {`/products/${id}`} ><img src={image} alt={alt} /></Link>
-      
+      <Link to={`/products/${id}`}>
+        <img src={image} alt={alt} />
+      </Link>
 
-      <button>Add to Cart</button>
+      <button onClick={() => handleAddProduct(id)}>Add to Cart</button>
     </div>
   );
 }
