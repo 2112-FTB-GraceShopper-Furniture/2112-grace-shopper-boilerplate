c4e609571340cb8c668cd420e003f2e415bd8db8
Ramses Angles <revolio@Anvil.localdomain>
2022-06-07 21:37:52 -0500

    fixed some styleing bugs and styled home page

--- src/components/CheckoutPage.js
+++ src/components/CheckoutPage.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../style/ThankYou.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../style/ThankYou.css";
 
 const CheckoutPage = () => {
 	return (
-		<div className='thankyou__hero'>
-			<h1 className='thankyou__header'>Order Submitted successfully!!</h1>
-			<h3 className='thankyou__subheader'>Your order will ship shortly</h3>
-			<Link to='/'> Go Back to Home Page </Link>
+		<div className="thankyou__hero">
+			<div className="thankyou__hero__container">
+				<h1>Order Submitted successfully!!</h1>
+				<h2>Your order will ship shortly</h2>
+				<Link to="/">
+					<button className="thankyou__button">Go Back to Home Page </button>
+				</Link>
+			</div>
 		</div>
 	);
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../style/Home.css";
 import FeaturedProduct from "./FeaturedProduct";
+import { Link } from "react-router-dom";
 
 
 const Hero = "/Assets/Home Hero Image.jpg";
@@ -8,15 +9,22 @@ const Hero = "/Assets/Home Hero Image.jpg";
 function Home() {
   return (
     <div className="home">
+      <div className="home__hero">
+        <div className="home__hero__container">
+          <h1>Welcome to Coconut Furniture</h1>
+          <h2>From house to home a click away</h2>
+          <Link to='/shop'>
+            <button className="home__hero__container__button">Shop All Funiture</button>
+          </Link >
+        </div>
+      </div>
       <div className="home__container">
-        <img
-          className="home__image"
-          src={Hero}
-          alt="Beautiful modern patio furniture with some green shrubbery on the foreground"
-        />
-
+        <div className="home__container__heading">
+          <h2>See our featured products</h2>
+        </div>
         <div className="home__row">
           <FeaturedProduct
+            key={1}
             id="1"
             title="Test Name"
             price={599.99}
@@ -24,6 +32,7 @@ function Home() {
             rating={5}
           />
           <FeaturedProduct
+            key={2}
             id="2"
             title="Modern End Table"
             price={199.95}
@@ -33,6 +42,7 @@ function Home() {
         </div>
         <div className="home__row">
           <FeaturedProduct
+            key={3}
             id="3"
             title="Antique Wooden Chair"
             price={99.95}
@@ -40,6 +50,7 @@ function Home() {
             rating={4}
           />
           <FeaturedProduct
+            key={4}
             id="4"
             title="Stylish End Table Sets"
             price={199.95}
@@ -47,6 +58,7 @@ function Home() {
             rating={5}
           />
           <FeaturedProduct
+            key={5}
             id="5"
             title="Child Wooden Night Stand"
             price={279.95}
@@ -57,6 +69,7 @@ function Home() {
 
         <div className="home__row">
           <FeaturedProduct
+            key={6}
             id="6"
             title="Bobby Living Room Set"
             price={1279.95}
@@ -64,6 +77,9 @@ function Home() {
             rating={5}
           />
         </div>
+      </div>
+      <div>
+        <h1>Testing</h1>
       </div>
     </div>
   );
